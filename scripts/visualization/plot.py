66376993a63657c520cccb4d681ac8c72b3154e6
He Lei <598269931@qq.com>
2019-10-18 17:26:18 +0100

    update plot

--- scripts/visualization/plot.py
+++ scripts/visualization/plot.py
@@ -5,49 +5,70 @@ import seaborn as sns
 
 def plot_q_value_max_heatmap(data_path):
     q_value_max = np.load('scripts/visualization/' + str(data_path) + '/q_value_max.npy')
-    q_value_max[q_value_max==-100] = 0
-
+    # q_value_max[q_value_max==-100] = 0
+    np.savetxt('scripts/visualization/' + str(data_path) + '/q_value_max.csv', q_value_max, delimiter=',', fmt='%2.1f')
     sns.set()
-    sns.heatmap(q_value_max.T, cmap="rainbow").invert_yaxis()
+    sns.heatmap(q_value_max.T, vmin=0, cmap="rainbow", xticklabels=10, yticklabels=10).invert_yaxis()
     plt.show()
 
-def plot_action():
-    action1 = np.load('scripts/visualization/goal1/action1.npy')
-    action2 = np.load('scripts/visualization/goal1/action2.npy')
-    q_values = np.load('scripts/visualization/goal1/q_values.npy')
+def plot_action(data_path):
+    data_path_str = 'scripts/visualization/' + str(data_path)
+    action1 = np.load(data_path_str + '/action1.npy')
+    action2 = np.load(data_path_str + '/action2.npy')
 
-    plt.figure()
+    fig_speed = plt.figure(figsize=(18, 6))
     for i in range(10):
-        plt.subplot(2, 5, i+1)
+        plt.subplot(2, 5, i+1, title='yaw={:d}(degrees)'.format(36 * i))
         sns.set()
-        sns.heatmap(action1[:, :, i].T, cmap="rainbow").invert_yaxis()
+        if i < 5:
+            if i == 0:
+                sns.heatmap(action1[:, :, i].T, cmap="rainbow", xticklabels=False, yticklabels=10).invert_yaxis()
+            else:
+                sns.heatmap(action1[:, :, i].T, cmap="rainbow", xticklabels=False, yticklabels=False).invert_yaxis()
+        else:
+            if i == 5:
+                sns.heatmap(action1[:, :, i].T, cmap="rainbow", xticklabels=10, yticklabels=10).invert_yaxis()
+            else:
+                sns.heatmap(action1[:, :, i].T, cmap="rainbow", xticklabels=10, yticklabels=False).invert_yaxis()
+    fig_speed.suptitle('Speed command for each state')
 
-    plt.figure()
+    fig_rotate = plt.figure(figsize=(18, 6))
     for i in range(10):
-        plt.subplot(2, 5, i+1)
+        plt.subplot(2, 5, i+1, title='yaw={:d}(degrees)'.format(36 * i))
         sns.set()
-        sns.heatmap(action2[:, :, i].T, cmap="rainbow").invert_yaxis()
+        if i < 5:
+            if i == 0:
+                sns.heatmap(action2[:, :, i].T, cmap="rainbow", xticklabels=False, yticklabels=10).invert_yaxis()
+            else:
+                sns.heatmap(action2[:, :, i].T, cmap="rainbow", xticklabels=False, yticklabels=False).invert_yaxis()
+        else:
+            if i == 5:
+                sns.heatmap(action2[:, :, i].T, cmap="rainbow", xticklabels=10, yticklabels=10).invert_yaxis()
+            else:
+                sns.heatmap(action2[:, :, i].T, cmap="rainbow", xticklabels=10, yticklabels=False).invert_yaxis()
 
+    fig_rotate.suptitle('Angular velocity command for each state')
     plt.show()
 
 def plot_q_value_max_heatmap_multi():
     """ plot q_value_max heatmap for all models of end point -1 9 """
 
-    plt.figure()
+    plt.figure(figsize=(18, 7))
     # load data
     for i in range(8):
         if i == 0:
-            data_path = 'scripts/visualization/model-{:d}'.format(800)
+            data_path = 'scripts/visualization/model-{:d}'.format(i)
         else:
             data_path = 'scripts/visualization/model-{:d}'.format(i * 100)
         q_value_max = np.load(data_path + '/q_value_max.npy')
-        plt.subplot(2, 4, i+1)
+
+        plt.subplot(2, 4, i+1, title='model-{:d}'.format(i*100))
         sns.set()
-        sns.heatmap(q_value_max.T, vmin=0, vmax=12,cmap="rainbow").invert_yaxis()
+        sns.heatmap(q_value_max.T, vmin=0, cmap="rainbow", xticklabels=5, yticklabels=5).invert_yaxis()
 
     plt.show()
 
 if __name__ == "__main__":
-    plot_q_value_max_heatmap('model-700_goal_-1_9')
-    # plot_action()
+    # plot_q_value_max_heatmap('model-700-detail')
+    plot_action('model-700_-5_5')
     # plot_q_value_max_heatmap_multi()
